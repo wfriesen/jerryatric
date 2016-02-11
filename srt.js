7a3437da04ee1fbbbc1d72ebb2f9f75d29179069
William Friesen <wfriesen@gmail.com>
2016-02-11 19:50:49 +1100

    Added subtitle parsing

---
+++ srt.js
@@ -0,0 +1,21 @@
+var parser = require('subtitles-parser');
+var http = require('http');
+var fs = require('fs');
+var execSync = require('child_process').execSync;
+
+var fileList = fs.readdir('./subs', function(err, files) {
+  files.forEach(function(file) {
+
+    var srt = fs.readFileSync('./subs/' + file, "utf8");
+    var subs = parser.fromSrt(srt);
+
+    subs.forEach(function(sub) {
+      sub.episode = file;
+      var filename = './json/' + file + '.' + sub.id + '.json';
+      console.log(filename);
+
+      // Need to be synchronous here to avoid "Too many open files" error
+      fs.writeFileSync(filename, JSON.stringify(sub));
+    });
+  });
+});
