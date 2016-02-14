90b7a5479a9ad331a81ff46db12d041a4a266fd3
William Friesen <wfriesen@gmail.com>
2016-02-14 17:28:03 +1100

    Removed unused routes

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -9,12 +9,7 @@
 
 import React from 'react';
 import Router from 'react-routing/src/Router';
-import fetch from './core/fetch';
 import App from './components/App';
-import ContentPage from './components/ContentPage';
-import ContactPage from './components/ContactPage';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorPage from './components/ErrorPage';
 import SearchPage from './components/SearchPage';
@@ -26,18 +21,6 @@ const router = new Router(on => {
   });
 
   on('/', async () => <SearchPage />);
-
-  on('/contact', async () => <ContactPage />);
-
-  on('/login', async () => <LoginPage />);
-
-  on('/register', async () => <RegisterPage />);
-
-  on('*', async (state) => {
-    const response = await fetch(`/api/content?path=${state.path}`);
-    const content = await response.json();
-    return content && <ContentPage {...content} />;
-  });
 
   on('error', (state, error) => state.statusCode === 404 ?
     <App context={state.context} error={error}><NotFoundPage /></App> :
